5439d72b6863cdfd0f6dd6ed702fd5439d4d1f33
phelisa2020 <phelisagqwetha@gmail.com>
2020-07-02 17:34:13 +0200

    my commits

--- greet.js
+++ greet.js
@@ -2,13 +2,14 @@ var nameBtnElem = document.querySelector('.nameBtn')
 var theMessageElem = document.querySelector('.message')
 var nameEnteredElem = document.querySelector('.nameEntered')
 var counterElem = document.querySelector('.counter')
-var greetCounter = [];
-
+var counter = [{'name':'pearl'}, {'name':'lisa'}, {'name':'some'}, {'name':'kunga'}, {'name':'ali'} ];
+var userName = nameEnteredElem.value;
+// var data = localStorage.getItem('name') ? JSON.parse(localStorage.getItem('name') : [])
 function greetMe() {
 	var languageElem = document.querySelector("input[name='languageType']:checked");
 
  	var languagePicked = languageElem.value;
- 	var userName = nameEnteredElem.value;
+ 	
  	 nameEnteredElem.value = ''
 
  	if(languagePicked === 'English'){
@@ -30,13 +31,27 @@ function greetMe() {
  
 	nameBtnElem.addEventListener('click', greetMe);
 	function upDateCounter(){	
-	if(nameEnteredElem === ''){
- 	 	return 
+	if(name[userName] === undefined){
+ 	 	counter ++
+ 	
+ 	  name[userName] = 0;
+ 	 counterElem.innerHTML = counter
+ 	 	
  	 }
- 	 else {
- 	 	greetCounter ++
- 	 	return counterElem.innerHTML = greetCounter
- 	 console.log(greetings)	
- 	 }
+ 	 
+   
  	 }	
  	 nameBtnElem.addEventListener('click', upDateCounter);
+
+function objectToString (){
+	 let counterObjToString = JSON.stringify(counter)
+	 console.log(typeof counterObjToString)
+	// 	'name': nameEnteredElem.value
+	// }
+	localStorage.setItem('counter', counterObjToString)
+	// greetCounter.push('allNames');
+	var toJsonCounter = JSON.parse(counterObjToString)
+	 
+	 console.log(typeof toJsonCounter)
+}
+ nameBtnElem.addEventListener('click', objectToString);
